0abadeb4e084db4db0f5d314cfa780cda2fb86e1
Zed <35542555+Zedstar16@users.noreply.github.com>
2018-10-07 12:03:16 +0100

    Added files from NoVoid repo
    done this so plugin can be submitted on poggit, it wasnt working with the old repo due to a bug?

---
+++ src/Zedstar16/NoVoid/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zedstar16\NoVoid;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\math\Vector3;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\entity\Entity;
+class Main extends PluginBase implements Listener{
+
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info(TextFormat::GREEN . "NoVoid by Zed enabled!");
+        $this->saveResource("config.yml");
+    }
+
+     public function onMove(PlayerMoveEvent $event): void{
+        if($event->getTo()->getFloorY() < 0){
+            $player = $event->getPlayer();
+
+            $config = $this->getConfig();
+            $coords = $config->get("teleport-to-default-spawn") ? $this->getServer()->getDefaultLevel()->getSafeSpawn() : new Position((int)$config->get("spawn-x"), (int)$config->get("spawn-y"), (int)$config->get("spawn-z"), $this->getServer()->getLevelByName((string)$config->get("spawn-level"))); // (int) and (string) for PhpStorm reasons
+            $player->teleport($coords);
+            
+
+            $level = $this->getServer()->getDefaultLevel();
+            $spawn = $level->getSafeSpawn();
+            $x = $spawn->getFloorX();
+            $y = $spawn->getFloorY();
+            $z = $spawn->getFloorZ();
+            
+            if($this->getConfig()->get("teleport-to-default-spawn")){
+               $player->teleport(new Position($x, $y, $z, $level));
+            }else{
+                $player->teleport(new Vector3($this->getConfig()->get("spawn-x"), $this->getConfig()->get("spawn-y"), $this->getConfig()->get("spawn-z"), $this->getConfig()->get("spawn-level")));
+            }
+            $player->sendMessage($config->get("message"));
+
+        }
+    }
+
+    public function onDisable() : void{
+        $this->getLogger()->info("NoVoid disabled");
+    }
+}
